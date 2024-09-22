32bf842baf7438cdca4b770a20abe041459dbc7b
Gabriel Ferreira <gabrielvictor.gferreira@gmail.com>
2024-09-21 19:28:50 -0500

    Organizing hierarchy

---
+++ homework/homework_2/code/solution.py
@@ -0,0 +1,39 @@
+from sklearn.svm import SVC
+
+
+def svm_with_diff_c(train_label, train_data, test_label, test_data):
+    '''
+    Use different value of cost c to train a svm model. Then apply the trained model
+    on testing label and data.
+    
+    The value of cost c you need to try is listing as follow:
+    c = [0.01, 0.1, 1, 2, 3, 5]
+    Please set kernel to 'linear' and keep other parameter options as default.
+    No return value is needed
+    '''
+
+    ### YOUR CODE HERE
+
+
+
+    ### END YOUR CODE
+    
+
+def svm_with_diff_kernel(train_label, train_data, test_label, test_data):
+    '''
+    Use different kernel to train a svm model. Then apply the trained model
+    on testing label and data.
+    
+    The kernel you need to try is listing as follow:
+    'linear': linear kernel
+    'poly': polynomial kernel
+    'rbf': radial basis function kernel
+    Please keep other parameter options as default.
+    No return value is needed
+    '''
+
+    ### YOUR CODE HERE
+
+
+
+    ### END YOUR CODE
